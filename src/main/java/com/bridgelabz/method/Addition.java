28fcf1201c07e086d286d06f74e195a2b838260c
Nileshlad1111 <nileshcareer1111@gmail.com>
2021-05-21 06:30:32 +0530

    ADDED:To java methods and method passing value and method call

---
+++ src/main/java/com/bridgelabz/method/Addition.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.method;
+
+public class Addition {
+    int sum = 0;
+
+    public int addTwoInt(int a, int b){
+
+        // adding two integer value.
+        sum = a + b;
+
+        //returning summation of two values.
+        return sum;
+    }
+}
