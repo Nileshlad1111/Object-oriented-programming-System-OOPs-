54110ea59698be9e863a3799774862efa5194475
Nileshlad1111 <nileshcareer1111@gmail.com>
2021-05-21 06:11:00 +0530

    ADDED:To create class and object to example of constructor ,instance veriable,set and get methods

---
+++ src/main/java/com/bridgelabz/classandobject/Dog.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.classandobject;
+
+public class Dog {
+
+    // Instance Variables
+    String name;
+    String breed;
+    int age;
+    String color;
+
+
+    public static void main(String[] args) {
+
+    }
+}
