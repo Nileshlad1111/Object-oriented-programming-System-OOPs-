8e25e966953bec53380cb232e02d2a4cc0ff36f2
Nileshlad1111 <nileshcareer1111@gmail.com>
2021-05-21 06:25:57 +0530

    ADDED:To add the class ,object ,methods,constructor ,override method as simple example

--- src/main/java/com/bridgelabz/classandobject/Dog.java
+++ src/main/java/com/bridgelabz/classandobject/Dog.java
@@ -8,8 +8,60 @@ public class Dog {
     int age;
     String color;
 
+    //constructor
+    public Dog(String name, String breed, int age, String color) {
+        this.name = name;
+        this.breed = breed;
+        this.age = age;
+        this.color = color;
+    }
+
+    //getter and setter
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return("Hi my name is "+ this.getName()+
+                ".\nMy breed,age and color are " +
+                this.getBreed()+"," + this.getAge()+
+                ","+ this.getColor());
+    }
 
     public static void main(String[] args) {
+
+        Dog tuffy = new Dog("Moatey","papillon", 5, "white");
+        System.out.println(tuffy.toString());
 
     }
 }
---
+++ src/test/java/com/bridgelabz/classandobject/Dog.java
@@ -0,0 +1,5 @@
+package com.bridgelabz.classandobject;
+
+public class Dog {
+    
+}
