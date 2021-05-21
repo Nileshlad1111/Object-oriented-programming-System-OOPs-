7dbb4fc6d8eff7abd1499a3e8ac047207a9edb6b
Nileshlad1111 <nileshcareer1111@gmail.com>
2021-05-21 06:01:51 +0530

    ADDED:To cover the oops concepts

---
+++ src/main/java/com/bridgelabz/OOPs.java
@@ -0,0 +1,11 @@
+package com.bridgelabz;
+
+public class OOPs {
+    public static void main(String[] args) {
+        System.out.println("1) Class. The class is a group of similar entities. ..." +
+                " 2) Object. An object can be defined as an instance of a class, and " +
+                "there can be multiple instances of a class in a program. ..." +
+                " 3) Inheritance. ... 4) Polymorphism. ... 5) Abstraction. ..." +
+                " 6) Encapsulation. ... 7) Association. ... 8) Aggregation.");
+    }
+}
