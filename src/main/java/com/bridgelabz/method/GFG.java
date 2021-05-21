80ad5cda7381388618f668bc4c2233a382b95bca
Nileshlad1111 <nileshcareer1111@gmail.com>
2021-05-21 06:33:40 +0530

    ADDED:To method calling

--- src/main/java/com/bridgelabz/method/GFG.java
+++ src/main/java/com/bridgelabz/method/GFG.java
@@ -3,13 +3,18 @@ package com.bridgelabz.method;
 public class GFG {
     public static void main (String[] args) {
 
-        // creating an instance of Addition class
-        Addition add = new Addition();
+        // Creating an instance of the class
+        Test obj = new Test();
 
-        // calling addTwoInt() method to add two integer using instance created
-        // in above step.
-        int s = add.addTwoInt(1,2);
-        System.out.println("Sum of two integer values :"+ s);
+        // Calling the m1() method by the object created in above step.
+        int i = obj.m1();
+        System.out.println("Control returned after method m1 :" + i);
 
+        // Call m2() method
+        // obj.m2();
+        int no_of_objects = Test.get();
+
+        System.out.print("No of instances created till now : ");
+        System.out.println(no_of_objects);
     }
 }
